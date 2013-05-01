f32123f0b8594043f01d36efda08e1a206ae1c02
Rob Colby <rcolby@me.com>
2013-05-01 11:19:45 -0700

    Added Checkbox

--- app/helpers/todo_items_helper.rb
+++ app/helpers/todo_items_helper.rb
@@ -1,2 +1,5 @@
 module TodoItemsHelper
+  def svg_object(name)
+    "<span><object data=\"#{image_path(name + '.svg')}\" type=\"image/svg+xml\">#{image_tag(name + '.png')}</object></span>".html_safe
+  end
 end
